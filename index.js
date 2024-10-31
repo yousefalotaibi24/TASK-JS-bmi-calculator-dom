c6b0fc108e089d02d6894567d8458a4c87498ceb
yosf AL-otaibi <yousefalotaini21@gmail.com>
2024-10-31 17:46:07 +0300

    task done

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  const heightInMeter = height / 100;
+  const BMI = weight / (heightInMeter * heightInMeter);
+  alert(BMI);
+  if (BMI < 18.5) {
+    alert("underweight");
+  } else if (BMI >= 18.5 && BMI < 25) {
+    alert("Healthy Weight");
+  } else if (BMI >= 25 && BMI < 30) {
+    alert("Overweight");
+  } else if (BMI >= 30) {
+    alert("Obesity");
+  }
 }
